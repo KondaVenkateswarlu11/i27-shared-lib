bcb585fec763f5582c952e1793f8da449b0fd642
Venkat <venkat@123>
2024-04-17 17:45:43 +0530

    ghgh

--- src/com/i27academy/kubernetes/K8s.groovy
+++ src/com/i27academy/kubernetes/K8s.groovy
@@ -7,15 +7,15 @@ class K8s {
     }
 
     def auth_login_eks() {
-    jenkins.sh """#!/bin/bash
-    echo "Entering Authentication method for EKS Cluster Login"
-    # Update kubeconfig with EKS cluster details
-    aws eks --region us-east-1 update-kubeconfig --name clothingCluster
-    echo "************* Listing Number of Nodes in EKS *************"
-    # List the nodes in the EKS cluster
-    kubectl get nodes
-    kubectl get ns
-    """
+        jenkins.sh """#!/bin/bash
+        echo "Entering Authentication method for EKS Cluster Login"
+        # Update kubeconfig with EKS cluster details
+        aws eks --region us-east-1 update-kubeconfig --name clothingCluster
+        echo "************* Listing Number of Nodes in EKS *************"
+        # List the nodes in the EKS cluster
+        kubectl get nodes
+        kubectl get ns
+        """
     }
 
 }
