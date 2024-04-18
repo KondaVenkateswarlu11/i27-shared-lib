91f8312c8b1862785227fa8c56aec50be20d68b0
Venkat <venkat@123>
2024-04-18 08:49:15 +0530

    Added K8sPipeline

---
+++ src/com/i27academy/kubernetes/K8s.groovy
@@ -0,0 +1,20 @@
+package com.i27academy.kubernetes
+
+class K8s{
+
+    def jenkins
+    K8s(jenkins){
+        this.jenkins = jenkins
+    }
+
+    def auth_login_eks(){
+        jenkins.sh """#!/bin/bash
+        echo "Entering Authentication method for EKS Cluster Login"
+        # Update kubeconfig with EKS cluster details
+        aws eks --region us-east-1 update-kubeconfig --name eurekaCluster
+        echo "************* Listing Number of Nodes in EKS *************"
+        # List the nodes in the EKS cluster
+        kubectl get nodes
+        """
+    }
+}
