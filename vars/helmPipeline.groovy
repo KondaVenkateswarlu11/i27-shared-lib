195beabf20d020e4590d9ab58276f5abb94383e3
Venkat <venkat@123>
2024-04-22 16:23:09 +0530

    completed k8s deploy methods getting error at image tag

--- vars/helmPipeline.groovy
+++ vars/helmPipeline.groovy
@@ -77,6 +77,10 @@ def call(Map pipelineParams) {
             STAGE_NAMESPACE = "cart-stage-ns"
             PROD_NAMESPACE = "cart-prod-ns"
 
+            DEV_ENV = "dev"
+            TST_ENV = "tst"
+            HELM_PATH = "${WORKSPACE}/i27-shared-lib/chart"
+
 
         }
         tools{
@@ -205,11 +209,13 @@ def call(Map pipelineParams) {
                 }
                 steps{
                     script{
+                        sh "ls -la"
+                        sh "ls -la .cicd"
                         imageValidation().call()
                         def docker_image = "${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${env.DOCKER_IMAGE_TAG}"
                         k8s.auth_login_eks("${env.EKS_DEV_CLUSTER_NAME}", "${env.EKS_DEV_REGION}")
                         //k8s.k8sdeploy("${env.K8S_DEV_FILE}", docker_image, "${env.DEV_NAMESPACE}")
-                        k8s.k8sHelmChartDeploy()
+                         k8s.k8sHelmChartDeploy("${env.APPLICATION_NAME}", "${env.DEV_ENV}", "${env.HELM_PATH}" , "${env.DOCKER_IMAGE_TAG}")
                         echo "Deployed to Dev Successfully!!!!!!!!!"
                     }
                 }
